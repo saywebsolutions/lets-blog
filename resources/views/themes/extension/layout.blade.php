afb3b46bb2bc72b03c0322657e088b9afcd7bd09
Kyle <kyle@saywebsolutions.com>
2019-11-02 00:02:49 -0400

    restore sidebar in extension theme

--- resources/views/themes/extension/layout.blade.php
+++ resources/views/themes/extension/layout.blade.php
@@ -23,9 +23,24 @@
 
             @else
 
+{{--
                 <div class="col-xs-12">
                     @section ('layout.view')
                         @include ($view)
+                    @show
+                </div>
+--}}
+
+                <div class="col-md-8 col-lg-9">
+                    @section ('layout.view')
+                        @include ($view)
+                    @show
+                </div>
+
+                <br class="hidden-md hidden-lg" style="clear:both;">
+
+                <div class="col-md-4 col-lg-3">
+                    @section ('layout.sidebar')
                     @show
                 </div>
 
